4d229e64d40877f8c7cab4a408081c06183606f8
Tiago Fernandes <tgfnds23@gmail.com>
2020-12-12 22:14:23 +0000

    styling and intro update

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className={styles.intro}>
       <div className={styles.intro__title}>Hey, I'm Tiago</div>
-      <p>Full-Stack Developer</p>
+      <p>Full-Stack Software Engineer</p>
       <p className={styles.intro__origin}>Portugal</p>
       <p>Graduated in Software Engineering in 2017</p>
       <p>I enjoy learning, coding, tech and more.</p>
